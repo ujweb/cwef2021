ce7ae5e798f21392ef519f9528c8ebd1f7d8f1bc
yutingh <yutingh@cw.com.tw>
2020-11-15 13:18:33 +0800

    renew

--- assets/js/style.js
+++ assets/js/style.js
@@ -26,6 +26,39 @@ $(function () {
             this.removeEventListener('touchstart', videoStart);
 		});
 	}
+	$('.hamburger').click(function(){
+		$(this).toggleClass('active');
+		$('nav > .container > ul').fadeToggle();
+	});
+	$('nav > .container > ul > li > a').click(function(){
+		$('.hamburger').removeClass('active');
+		$('nav > .container > ul').fadeOut();
+	});
+	$('.container-twice').each(function(){
+		let phraseHeight = $(this).children('.column-txt').children('.content').children('p').outerHeight(),
+			columnImg = $(this).children('.column-img'),
+			columnImgHeight = columnImg.outerHeight();
+		columnImg.css('padding-top', phraseHeight);
+		columnImg.children('.img').height(columnImgHeight-phraseHeight);
+	});
+	$('.slider-center').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		centerMode: true,
+		centerPadding: '0',
+		prevArrow: '<button type="button" class="slick-prev"><i class="icon icon-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="icon icon-chevron-right"></i></button>',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
 	$(window).on("scroll", function () {
 		var scroll = $(window).scrollTop();
 		if (scroll >= height / 3) {
